d986d443a573fd70c9adce5364c95f88866320f8
Jerome cognet <jcognet@gmail.com>
2016-11-12 16:23:23 +0100

    Mise à jour des fixtures

--- src/CommunBundle/DataFixtures/ORM/LoadUserData.php
+++ src/CommunBundle/DataFixtures/ORM/LoadUserData.php
@@ -3,6 +3,7 @@
 namespace CommunBundle\DataFixtures\ORM;
 
 use CommunBundle\Entity\Devise;
+use CommunBundle\Entity\SuiviDevise;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use UserBundle\Entity\User;
@@ -13,14 +14,21 @@ class LoadUserData implements FixtureInterface
     {
         $user = new User();
         $user->setEmail('jcognet@gmail.com')
-        ->setUsername('jcognet@gmail.com')
+            ->setUsername('jcognet@gmail.com')
             ->setUsernameCanonical('jcognet')
             ->setEnabled(true)
             ->setPlainPassword('jcognet@gmail.com')
             ->setPassword('jcognet@gmail.com')
-            ->setSuperAdmin(true)
-        ;
+            ->setSuperAdmin(true);
 
+        $suiviDevise = new SuiviDevise();
+        $suiviDevise->setSeuilMax(120)
+            ->setSeuilMin(110)
+            ->setUser($user)
+            ->setDevise($manager->getReference(Devise::class, 1));
+        $user->addListeDevise($suiviDevise);
+
+        $manager->persist($suiviDevise);
         $manager->persist($user);
 
         $manager->flush();

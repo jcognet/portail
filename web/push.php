1df85daa234a9cb8bc3b5cdecc8d73f443d9649f
Jerome cognet <jcognet@gmail.com>
2016-11-26 14:44:14 +0100

    push post

--- web/push.php
+++ web/push.php
@@ -4,7 +4,15 @@ require_once("../../github_key.php");
 ob_start();
 $now = new \DateTime();
 echo $now->format('d/m/Y h:i:s');
-var_dump($_POST['payload']);
+var_dump($_POST);
+var_dump($_GET);
 $result = ob_get_clean();
 file_put_contents ('toto.txt', $result);
+// Protection git hub
+$github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178', '50.57.231.61');
+if (in_array($_SERVER['REMOTE_ADDR'], $github_ips)) {
+    echo "ok";
+}else{
+    echo "Mauvais serveur : ".$_SERVER['REMOTE_ADDR'];
+}
 ?>

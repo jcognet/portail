4c703d8bdf717856dc3d202fa2f9bf60806f20c8
Jerome cognet <jcognet@gmail.com>
2016-11-26 13:53:38 +0100

    app.php pour serveur de dev

--- web/app.php
+++ web/app.php
@@ -12,7 +12,8 @@ if (
     (in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) ||   # cas du localhost
     isset($_GET['debug']) && $_GET['debug'] == 'proutprout' ||      # cas du debug forcé
     !is_bool(strpos($_SERVER['REQUEST_URI'], '/_wdt/'))     ||      # cas du profiler en bas
-    !is_bool(strpos($_SERVER['REQUEST_URI'], '/_profiler/'))        # Cas du profiler
+    !is_bool(strpos($_SERVER['REQUEST_URI'], '/_profiler/')) ||     # Cas du profiler
+    !is_bool(strpos($_SERVER['HTTP_HOST'], 'changesous.dev/'))       # Cas du serveur de dév'
 
 ) {
     $envProd = false;

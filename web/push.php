ee7c0acc3abc706d8abcbbb70c9b0a7215b4e091
Jerome cognet <jcognet@gmail.com>
2016-11-26 14:23:48 +0100

    push

--- web/push.php
+++ web/push.php
@@ -4,5 +4,5 @@ require_once("../../github_key.php");
 ob_start();
 var_dump($_POST);
 $result = ob_get_clean();
-file_put_contents ($result, 'toto.txt');
+file_put_contents ('toto.txt', $result);
 ?>

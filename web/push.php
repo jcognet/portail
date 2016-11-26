8d451c1773a17541985ffb109e91a92cf4eb312c
Jerome cognet <jcognet@gmail.com>
2016-11-26 15:58:39 +0100

    enfin

--- web/push.php
+++ web/push.php
@@ -2,19 +2,15 @@
 // Modif 5
 require_once("../../github_key.php");
 $now = new \DateTime();
-echo $now->format('d/m/Y h:i:s');
-$payload = json_decode(stripslashes($_POST['payload']));
-//var_dump($payload);
-//var_dump($_SERVER[ 'HTTP_X_HUB_SIGNATURE' ]);
-//var_dump( hash_hmac( 'sha1', $_POST['payload'], GIT_HUB_KEY, false ));
-if( 'sha1=' . hash_hmac( 'sha1', $_POST['payload'], GIT_HUB_KEY, false ) === $_SERVER[ 'HTTP_X_HUB_SIGNATURE' ]) { //
-    echo "secre";
-}
-
+echo $now->format('d/m/Y h:i:s')."<br/>";
 $post_data = file_get_contents('php://input');
-$signature = hash_hmac('sha1', $post_data, GIT_HUB_KEY);
+$signature = 'sha1='hash_hmac('sha1', $post_data, GIT_HUB_KEY);
+if($signature == $_SERVER[ 'HTTP_X_HUB_SIGNATURE' ]){
+    echo "ok";
+}
+/*
 var_dump($signature);
 var_dump($_SERVER[ 'HTTP_X_HUB_SIGNATURE' ]);
 var_dump($post_data);
-var_dump(json_decode( $post_data));
+var_dump(json_decode( $post_data))*/
 ?>

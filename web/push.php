ad6b72e47898ab6e5f2bf9eae801b0f90bcd87ff
Jerome cognet <jcognet@gmail.com>
2016-11-26 14:42:11 +0100

    push post

--- web/push.php
+++ web/push.php
@@ -2,7 +2,9 @@
 // Modif 4
 require_once("../../github_key.php");
 ob_start();
-var_dump($_POST);
+$now = new \DateTime();
+echo $now->format('d/m/Y h:i:s');
+var_dump($_POST['payload']);
 $result = ob_get_clean();
 file_put_contents ('toto.txt', $result);
 ?>

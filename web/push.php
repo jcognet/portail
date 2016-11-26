21960bbdd258eb206a60d6cdde06f10a1188edbb
Jerome cognet <jcognet@gmail.com>
2016-11-26 14:45:13 +0100

    push post

--- web/push.php
+++ web/push.php
@@ -9,7 +9,7 @@ var_dump($_GET);
 $result = ob_get_clean();
 file_put_contents ('toto.txt', $result);
 // Protection git hub
-$github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178', '50.57.231.61');
+$github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178', '50.57.231.61', '192.30.252.42');
 if (in_array($_SERVER['REMOTE_ADDR'], $github_ips)) {
     echo "ok";
 }else{

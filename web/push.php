0b20b2e1e2a354266c54ad84379981f2c06671b1
Jerome cognet <jcognet@gmail.com>
2016-11-26 14:59:41 +0100

    push post

--- web/push.php
+++ web/push.php
@@ -5,6 +5,8 @@ $now = new \DateTime();
 echo $now->format('d/m/Y h:i:s');
 var_dump($_POST);
 var_dump($_GET);
+var_dump($_REQUEST);
+var_dump($_SERVER);
 $payload = json_decode(stripslashes($_REQUEST['payload']));
 var_dump($payload);
 

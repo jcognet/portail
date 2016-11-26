7f520f9834e1d43529fb3e97e111e3d8ad7dbd89
Jerome cognet <jcognet@gmail.com>
2016-11-26 15:59:16 +0100

    enfin

--- web/push.php
+++ web/push.php
@@ -4,7 +4,7 @@ require_once("../../github_key.php");
 $now = new \DateTime();
 echo $now->format('d/m/Y h:i:s')."<br/>";
 $post_data = file_get_contents('php://input');
-$signature = 'sha1='hash_hmac('sha1', $post_data, GIT_HUB_KEY);
+$signature = 'sha1='.hash_hmac('sha1', $post_data, GIT_HUB_KEY);
 if($signature == $_SERVER[ 'HTTP_X_HUB_SIGNATURE' ]){
     echo "ok";
 }

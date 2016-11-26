2f08d9e4e8782e30108dfddf3062c862bd3a726f
Jerome cognet <jcognet@gmail.com>
2016-11-26 15:05:19 +0100

    push

--- web/push.php
+++ web/push.php
@@ -3,11 +3,6 @@
 require_once("../../github_key.php");
 $now = new \DateTime();
 echo $now->format('d/m/Y h:i:s');
-var_dump($_POST);
-var_dump($_GET);
-var_dump($_REQUEST);
-var_dump($_SERVER);
-$payload = json_decode(stripslashes($_REQUEST['payload']));
+$payload = json_decode(stripslashes($_POST['payload']));
 var_dump($payload);
-
 ?>

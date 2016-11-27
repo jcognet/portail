8f39544b37613299688117bdd8ec20e5187df3f5
Jerome cognet <jcognet@gmail.com>
2016-11-27 21:05:06 +0100

    push

--- web/push.php
+++ web/push.php
@@ -4,12 +4,12 @@ $now = new \DateTime();
 echo $now->format('d/m/Y H:i:s') . "\n\n";
 $post_data = file_get_contents('php://input');
 $signature = 'sha1=' . hash_hmac('sha1', $post_data, GIT_HUB_KEY);
-echo $signature."<br/>";
-echo $_SERVER['HTTP_X_HUB_SIGNATURE']."<br/>";
+echo $signature."\n";
+echo $_SERVER['HTTP_X_HUB_SIGNATURE']."\n";
 if ($signature == $_SERVER['HTTP_X_HUB_SIGNATURE']) {
     $jsonDecode = json_decode($post_data);
     if ($jsonDecode->ref == "refs/heads/dev") {
-        echo "Lancement de la commande de mise en production pour la branche développement";
+        echo "Lancement de la commande de mise en production pour la branche développement\n";
         exec('../sh/dev_mep.sh');
     }
 }

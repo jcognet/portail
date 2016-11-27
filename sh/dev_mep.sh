f492a35246298dba89d7241721b9c6f483fa15c6
Jerome cognet <jcognet@gmail.com>
2016-11-27 21:09:51 +0100

    Ajout mkdir backup

--- sh/dev_mep.sh
+++ sh/dev_mep.sh
@@ -1,6 +1,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 DATE=$(date +"%Y_%_m_%d_%H_%M_%S")
+mkdir ../backup/
 ./mep.sh dev > ../backup/"$DATE"_mep.txt
 value=`cat ../backup/"$DATE"_mep.txt`
 echo "$value"

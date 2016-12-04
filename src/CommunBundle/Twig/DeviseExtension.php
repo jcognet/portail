ea50afe0026b594b4eae9562e5216c520f3f68a7
Jerome cognet <jcognet@gmail.com>
2016-12-04 18:47:57 +0100

    Gestion du format monnétaire

--- src/CommunBundle/Twig/DeviseExtension.php
+++ src/CommunBundle/Twig/DeviseExtension.php
@@ -17,13 +17,23 @@ class DeviseExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            new \Twig_SimpleFunction('affichePrix', array($this, 'affichePrix')),
+            new \Twig_SimpleFunction('affiche_prix', array($this, 'affichePrix')),
         );
     }
 
+    /**
+     * Affiche le prix d'une devise
+     * @param $valeur
+     * @return float
+     */
     public function affichePrix($valeur)
     {
-        return round($valeur, 2, PHP_ROUND_HALF_DOWN);
+        $number = number_format(round($valeur, 2, PHP_ROUND_HALF_DOWN), 2, ',', ' ');
+        // Suppression de ,00 final si nécessaire
+        if (substr($number, -2) == '00') {
+            $number = substr($number, 0, strlen($number) - 3);
+        }
+        return $number;
     }
 
 }

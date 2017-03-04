0ca894c4bf412d2337576ed5a91ba7d0ed3e1ed6
Jerome cognet <jcognet@gmail.com>
2017-03-04 10:30:59 +0100

    Gestion des news

--- web/js/news_liste.js
+++ web/js/news_liste.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
+    $('.news_content').hide();
     addEventTr();
 });
 
 function addEventTr(){
     $("#table_news tr").on('click', function(e){
         if($(this)[0].hasAttribute('data-news-id')){
-            var divNews = $(this).attr('data-news-id');
-            divNews.toggle();
+            var divNews = $('#news_content_'+$(this).attr('data-news-id'));
+            divNews.fadeToggle();
         }
     });
 }

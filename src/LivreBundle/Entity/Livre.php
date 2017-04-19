e1bef525d529355426041d5ede418e2ccd31a993
Jerome <jcognet@gmail.com>
2017-04-19 16:54:15 +0200

    Gestion livre

---
+++ src/LivreBundle/Entity/Livre.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace LivreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Livre
+ *
+ * @ORM\Table(name="livre")
+ * @ORM\Entity(repositoryClass="LivreBundle\Repository\LivreRepository")
+ */
+class Livre
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateAjout", type="datetime")
+     */
+    private $dateAjout;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="prix", type="float", nullable=true)
+     */
+    private $prix;
+
+    /**
+     * @var \UserBundle\Entity\User
+     *
+     * @ORM\ManyToOne(targetEntity="UserBundle\Entity\User")
+     */
+    private $proprietaire;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set dateAjout
+     *
+     * @param \DateTime $dateAjout
+     *
+     * @return Livre
+     */
+    public function setDateAjout($dateAjout)
+    {
+        $this->dateAjout = $dateAjout;
+
+        return $this;
+    }
+
+    /**
+     * Get dateAjout
+     *
+     * @return \DateTime
+     */
+    public function getDateAjout()
+    {
+        return $this->dateAjout;
+    }
+
+    /**
+     * Set dateAction
+     *
+     * @param \DateTime $dateAction
+     *
+     * @return Livre
+     */
+    public function setDateAction($dateAction)
+    {
+        $this->dateAction = $dateAction;
+
+        return $this;
+    }
+
+    /**
+     * Get dateAction
+     *
+     * @return \DateTime
+     */
+    public function getDateAction()
+    {
+        return $this->dateAction;
+    }
+
+    /**
+     * Set action
+     *
+     * @param string $action
+     *
+     * @return Livre
+     */
+    public function setAction($action)
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+
+    /**
+     * Get action
+     *
+     * @return string
+     */
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    /**
+     * Set prix
+     *
+     * @param float $prix
+     *
+     * @return Livre
+     */
+    public function setPrix($prix)
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    /**
+     * Get prix
+     *
+     * @return float
+     */
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    private function __construct()
+    {
+        $this->dateAjout = new \DateTime();
+    }
+}
+

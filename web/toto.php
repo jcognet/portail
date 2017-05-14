e3f1d8b6510e6708c4b397aa37d6af3d17d57492
Jerome cognet <jcognet@gmail.com>
2017-05-14 20:16:45 +0200

    ajout jquery

--- web/toto.php
+++ web/toto.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>WebCodeCamJS</title>
+    <script src="/js/jquery-3.1.1.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="/css/bootstrap.min.css" />
 </head>

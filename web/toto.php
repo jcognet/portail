dcbe24e144bcdc141ef896d156b844f282de0449
Jerome cognet <jcognet@gmail.com>
2017-05-14 20:03:08 +0200

    test page à part

---
+++ web/toto.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+<h3>Simple initialization with default settings</h3>
+<hr>
+<canvas></canvas>
+<hr>
+<select></select>
+<ul></ul>
+<script type="text/javascript" src="/edsa-changesous/js/qrcodelib.js"></script>
+<script>
+    var DECODER_WORKER_ASSET = "/edsa-changesous/js/DecoderWorker.js";
+    var AUDIO_ASSET = "/edsa-changesous/audio/beep.mp3";
+</script>
+<script type="text/javascript" src="/edsa-changesous/js/webcodecamjs.js"></script>
+<script type="text/javascript" src="/edsa-changesous/js/isbn.js"></script>
+</body>
+</html>
